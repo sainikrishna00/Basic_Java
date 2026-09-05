bb3a8fa2428a96ac51fc5ba0d1da567e9aa5eb68
Krishna Saini <sainikrishna93304@gmail.com>
2026-09-06 01:24:13 +0530

    switch program

--- src/Switch.java
+++ src/Switch.java
@@ -9,15 +9,22 @@ public class Switch {
         num1 = sc.nextDouble();
         System.out.println("enter num2 : ");
         num2 = sc.nextDouble();
-        System.out.println("enter num2 : ");
+        System.out.println("enter operator : ");
         System.out.println("1 : +\n2 : -\n3 : *\n4 : /");
         int op = sc.nextInt();
-        switch (op){
-            case(1) -> System.out.println(num1 + num2);
-            case(2) -> System.out.println(num1 - num2);
-            case(3) -> System.out.println(num1 * num2);
-            case(4) -> System.out.println(num1 / num2);
+        switch (op) {
+            case (1) -> System.out.println(num1 + num2);
+            case (2) -> System.out.println(num1 - num2);
+            case (3) -> System.out.println(num1 * num2);
+            case (4) -> {
+                if (num2 == 0) {
+                    System.out.println("cannot divide by zero!");
+                } else {
+                    System.out.println(num1 / num2);
+                }
+            }
             default -> System.out.println("invalid input");
         }
+        sc.close();
     }
 }
