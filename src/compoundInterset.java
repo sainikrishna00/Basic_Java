c167ab59b061a83b01bb087ff04142ffb5351ff6
Krishna Saini <sainikrishna93304@gmail.com>
2026-09-05 19:43:00 +0530

    compound interest program using java basic

--- src/compoundInterset.java
+++ src/compoundInterset.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class compoundInterset {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        double principal;
+        double rate;
+        int timeCompound;
+        int year;
+        double amount;
+        System.out.println("Enter the principal amount: ");
+        principal = sc.nextDouble();
+        System.out.println("Enter the interet rate (in %) : ");
+        rate = sc.nextDouble() / 100;
+        System.out.print("Enter the # of times compound per year: ");
+        timeCompound = sc.nextInt();
+        System.out.print("Enter the # of year : ");
+        year = sc.nextInt();
+        amount = principal * Math.pow(1 + rate / timeCompound , timeCompound * year);
+        System.out.println("The amount after "+ year + " is : $" +amount);
+        sc.close();
+    }
 }
