7067c7e13a57c2a1cce37a0dbad382a78975cf45
Krishna Saini <sainikrishna93304@gmail.com>
2026-09-05 23:49:53 +0530

    temp convertor program

---
+++ src/tempConverter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class tempConverter {
+    static void main() {
+        double temp;
+        double newTemp;
+        String unit;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter temperature : ");
+        temp = sc.nextDouble();
+        System.out.println("convert to celcius or Fahrenheit ? (C or F):");
+        unit = sc.next().toUpperCase();
+        newTemp = (unit.equals("C")) ? (temp - 32) * 5/9 : (temp * 9 / 5) + 32;
+        System.out.println(newTemp + "°" + unit);
+        sc.close();
+    }
+}
