18f3ca52a7ac9a7b44083faf8feba1dd94570585
Krishna Saini <sainikrishna93304@gmail.com>
2026-09-06 17:31:08 +0530

    do-while java

---
+++ src/Do_while.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Do_while {
+    static void main() {
+        System.out.println("number gussing game!");
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        int guess;
+        int attempt = 0;
+        int randomNum = random.nextInt(1,11);
+        System.out.println("Guess a number between 1 - 10");
+        do{
+            System.out.println("enter a guess");
+            guess = sc.nextInt();
+            if(guess > 10){
+                System.out.println("Guess a number between 1 - 10");
+            }
+            attempt++;
+        }while(guess != randomNum);
+        System.out.printf("nice! you won in your %dth attempts!", attempt);
+        sc.close();
+    }
+}
